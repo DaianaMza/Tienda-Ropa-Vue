f1d3a24c95fdefbad24e8306fc1d1ab583cb1d62
Daiana <daianabusines@gmail.com>
2023-12-17 17:46:29 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
+<template>
+  <v-app>
+    <v-main>
+      <Navbar />
+      <v-container fluid>
+        <v-row>
+          <v-col cols="12">
+            <v-card>
+              <v-img src="./assets/verde.jpg" max-height="10%" max-width="100%" contain>
+                <v-card-title class="top ml-10">
+                  <h2 class="title1">NUEVA TEMPORADA</h2>
+                </v-card-title>
+              </v-img>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <article class="tarjetabody">
+          <figure><img src="https://pngimg.com/uploads/women_bag/women_bag_PNG6428.png" alt=""></figure>
+        </article>
+      </v-container>
+
+
+
+    </v-main>
+  </v-app>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.tarjetabody {
+  padding-top: 15px;
+}
+
+.tarjeta {
+  font-weight: 400;
+  margin: 2rem 0 0.25rem 0;
+}
+
+figure {
+  width: 400px;
+  aspect-ratio: 8 / 5;
+  --bg: hsl(330 80% calc(90% - (var(--hover) * 10%)));
+  --accent: hsl(129, 80%, 40%);
+  transition: background 0.2s;
+  background: radial-gradient(circle at top left, var(--accent), transparent 75%),
+    var(--bg);
+  margin: 0;
+  position: relative;
+  overflow: hidden;
+  border-radius: 1.5rem;
+}
+
+figure:after {
+  content: "";
+  position: absolute;
+  width: 20%;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  bottom: 0%;
+  left: 10%;
+  background: linear-gradient(-65deg, var(--bg) 50%, var(--accent) 50%);
+  filter: blur(25px);
+  transform:
+    translateX(calc(var(--hover) * 15%)) scale(calc(1 + (var(--hover) * 0.2)));
+  transition: transform 0.2s, background 0.2s;
+}
+
+img {
+  position: absolute;
+  left: 20%;
+  top: 15%;
+  width: 40%;
+  transform:
+    translateX(calc(var(--hover) * -15%)) scale(calc(1 + (var(--hover) * 0.2)));
+  transition: transform 0.2s;
+}
+article {
+  --hover: 0;
+}
+article:hover {
+  --hover: 1;
 }
 </style>
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    data() {
+        return {
+        };
+    },
+};
+</script>
+
+<template>
+    <v-app-bar app color="white" flat>
+        <v-badge color="#D5F0DB" dot>
+            <v-toolbar-title class="m1-4">
+                <span class="green--text">TU</span><strong>TIENDA </strong>
+            </v-toolbar-title>
+        </v-badge>
+        <v-spacer></v-spacer>
+        <v-badge  color="#white" >
+            <v-badge overlap content="3" class="mr-2 mt-1" color="#41AB55">
+                <v-avatar color="#FFF0EE" sixe="40">
+                    <v-icon x-small color="#41AB55">fas fa-shopping-cart</v-icon>
+                </v-avatar>
+            </v-badge>
+        </v-badge>
+    </v-app-bar>
+</template>
+  
+
+  
+<style scoped></style>
